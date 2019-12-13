0d790e0320fab2f48cc96b52aa2f967d30e297fa
brenno araujo queiroz <brennoaraujoqueiroz@gmail.com>
2019-12-13 14:40:13 -0300

    correcao no sucrase nodemon

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({
-    message: 'Hello World',
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'brenno araujo',
+    email: 'brenno@teste.co',
+    password_hash: '123123',
   });
+  return res.json(user);
 });
 
 export default routes;
