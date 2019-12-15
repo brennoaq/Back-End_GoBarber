09869d1dee982f4d4f0af5f930ed8bce95ade69d
brenno araujo queiroz <brennoaraujoqueiroz@gmail.com>
2019-12-15 20:03:50 -0300

    avatar do usuario

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionsController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+
 import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionsController.store);
@@ -11,4 +17,7 @@ routes.post('/sessions', SessionsController.store);
 routes.use(authMiddlewares);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
 export default routes;
