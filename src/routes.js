53280263531311a3e63754fab88cbebeb80b9030
brenno araujo queiroz <brennoaraujoqueiroz@gmail.com>
2019-12-14 21:47:10 -0300

    cadastro de usuarios

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'brenno araujo',
-    email: 'brenno@teste.co',
-    password_hash: '123123',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
