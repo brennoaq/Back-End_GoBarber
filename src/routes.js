aec172c64e97a4dea37328045c60af4e3a9c1d36
brenno araujo queiroz <brennoaraujoqueiroz@gmail.com>
2019-12-15 00:12:22 -0300

    autenticacao JWT

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionsController from './app/controllers/SessionController';
+import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionsController.store);
 
+routes.use(authMiddlewares);
+
+routes.put('/users', UserController.update);
 export default routes;
